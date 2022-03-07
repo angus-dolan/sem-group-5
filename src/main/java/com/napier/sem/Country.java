9d4c82e9f29e03418cbe67ecded4ad235c647c90
Julia Swietochowska <julkaswietochowska@gmail.com>
2022-03-07 16:29:06 +0000

    Update the class

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -7,15 +7,164 @@ public class Country {
     /**
      * Country's code, e.g. "GBR" -> "United Kingdom"
      */
-    public String code;
+    private String code;
 
     /**
      * Country's name
      */
-    public String name;
+    private String name;
 
     /**
      * Country's population
      */
-    public int population;
+    private int population;
+
+    /**
+     * Country's capital
+     */
+    private String capital;
+
+    /**
+     * Country's continent
+     */
+    private String continent;
+
+    /**
+     * Country's region
+     */
+    private String region;
+
+    /**
+     * Default empty constructor
+     */
+    public Country() {
+    }
+
+    /**
+     * Creates a Country objects with parameters passed.
+     * @param code  Country's code
+     * @param name  Country name
+     * @param continent Country's continent
+     * @param region    Country's region
+     */
+    public Country(String code, String name, String continent, String region) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+    }
+
+    /**
+     * Creates a Country objects with parameters passed.
+     * @param code  Country's code
+     * @param name  Country name
+     * @param continent Country's continent
+     * @param region    Country's region
+     * @param population    Country's population
+     * @param capital   Country's capital code
+     */
+    public Country(String code, String name, String continent, String region, int population, String capital) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.population = population;
+        this.capital = capital;
+    }
+
+    /**
+     * Gets the country's code
+     * @return  country code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Changes the value of the country's code
+     * @param code  country code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Gets the country name
+     * @return  country name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Changes the name of the country
+     * @param name  country name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the population of the country
+     * @return  country's population
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * Changes the population of the country
+     * @param population    population to set
+     */
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    /**
+     * Gets the country's capital
+     * @return  country's capital
+     */
+    public String getCapital() {
+        return capital;
+    }
+
+    /**
+     * Changes the country's capital
+     * @param capital   the country's capital to set
+     */
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    /**
+     * Gets the country's continent
+     * @return  country's continent
+     */
+    public String getContinent() {
+        return continent;
+    }
+
+    /**
+     * Changes the country's continent
+     * @param continent country's continent to set
+     */
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    /**
+     * Gets the country's region
+     * @return  country's region
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    /**
+     * Changes the country's region
+     * @param region    country's region to set
+     */
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
