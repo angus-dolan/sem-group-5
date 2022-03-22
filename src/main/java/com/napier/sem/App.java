675a43e1397f6bdbd6db76d48dc0af50f658e440
herrone <egretfulherron@gmail.com>
2022-03-22 12:22:11 +0000

    uncommented jacoco and fixed delay

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,9 +26,9 @@ public class App {
 
         // Connect to database
         if(args.length < 1){
-            a.connect("localhost:33060", 30000);
+            a.connect("localhost:33060", 0);
         }else{
-            a.connect(args[0], Integer.parseInt(args[1]));
+            a.connect("db:3306", 30000);
         }
 
         // Use case 2
